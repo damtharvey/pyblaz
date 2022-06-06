5a1e493a07d9df53d08827c5816a7c8fe0e5fc58
Tripti_Agarwal <31624673+tripti-agarwal@users.noreply.github.com>
2022-06-06 16:25:11 -0600

    decompression

--- compression.py
+++ compression.py
@@ -4,29 +4,45 @@ import pathlib
 import string
 from typing import Callable, Tuple, Union
 import tqdm
-
+import numpy as np
 import torch
 
 import transforms
 
+from scipy import fft
 
 def _test():
     dtype = torch.float64
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     compressor = Compressor(dtype=dtype, device=device)
-    x = torch.tensor([[0.01 * x * y for y in range(8)] for x in range(8)], dtype=dtype, device=device)
+    x = torch.tensor([[0.01 * x * y for y in range(16)] for x in range(8)], dtype=dtype, device=device)
     blocked = compressor.block(x)
     first_element, mean_slope, normalized_block = compressor.normalize(blocked[0, 0])
-    # print(normalized_block)
+    print(blocked[0][0])
 
     coefficients = compressor.block_transform(normalized_block)
-    print(coefficients)
-    coefficient_indices = compressor.predict(coefficients, mean_slope)
-    print(coefficient_indices)
-    print(coefficient_indices - 127)
+    #print(coefficients)
+    coefficient_indices, biggest_element = compressor.predict(coefficients, mean_slope)
+    #print(coefficient_indices - 127)
+    maximum_value = max(max(coefficient_indices, key=max))
+    
+    compressed_matrix = coefficient_indices - 127
+    added_back = compressed_matrix + 127
+    inverse_predict = compressor.unpredict(added_back, mean_slope, biggest_element)
+    #print((coefficients - inverse_predict).norm(torch.inf))
+    #print(inverse_predict)
+    inverse_DCT = compressor.block_transform(inverse_predict, inverse=True)
+    #print(inverse_DCT)
+    #print((normalized_block - inverse_DCT).norm(torch.inf))
 
+    inverse_normalized_part1 = inverse_DCT * mean_slope
+    #print(inverse_normalized_part1)
+    #print((blocked - inverse_normalized_part1).norm(torch.inf))
 
+    inverse_normalized_part2 = compressor.unnormalized(inverse_normalized_part1, first_element)
+    print(inverse_normalized_part2)
+    #print((blocked - inverse_normalized_part2).norm(torch.inf))
 class Compressor:
     """
     blaz compressor as in https://arxiv.org/abs/2202.13007
@@ -94,11 +110,34 @@ class Compressor:
         mean_slope = self.mean_slope(differences)
         return block[(0,) * self.n_dimensions], mean_slope, differences / mean_slope
 
+    def unnormalized(self, block: torch.Tensor, first_element: torch.float) -> torch.Tensor :
+        """
+        Section inverse IIb
+
+        :param block: a block of the input
+        :return: inverse of the normalized block
+        """
+        row, col = block.size()
+        new_array = np.zeros((row,col))
+        block_array = block.numpy()
+        for x in range(row):
+            for y in range(col):
+                if(x==0 and y ==0): 
+                    new_array[x][y] = first_element
+                elif(x==0 and y != 0):
+                    new_array[x][y] = block_array[x][y] + new_array[x][y-1]
+                elif(x != 0 and y == 0):
+                    new_array[x][y] = block_array[x][y] + new_array[x-1][y]
+                else:
+                    new_array[x][y] = ((block_array[x][y] + new_array[x-1][y]) + (block_array[x][y] + new_array[x][y-1]))/2 
+        
+        block_tensor =  torch.from_numpy(new_array)
+        return block_tensor
     @staticmethod
     def mean_slope(block: torch.Tensor) -> torch.float:
         return block.sum() / torch.count_nonzero(block)
 
-    def predict(self, normalized_block: torch.Tensor, mean_slope: torch.float) -> torch.Tensor:
+    def predict(self, normalized_block: torch.Tensor, mean_slope: torch.float) -> tuple[torch.Tensor, torch.float]:
         """
         Section IIc
 
@@ -114,8 +153,16 @@ class Compressor:
             dtype=self.dtype,
             device=self.device,
         )
-        return (normalized_block.unsqueeze(-1) - slopes).abs().min(-1).indices
-
+        return (normalized_block.unsqueeze(-1) - slopes).abs().min(-1).indices, biggest_element
+    def unpredict(self, predict_elements: torch.Tensor, mean_slope: torch.float, biggest_element) -> torch.Tensor:
+        slopes = torch.linspace(
+            mean_slope - biggest_element,
+            mean_slope + biggest_element,
+            self.n_bins,
+            dtype=self.dtype,
+            device=self.device,
+        )
+        return (slopes[predict_elements])
     def block_transform(
         self, slope_indices: torch.Tensor, n_coefficients: int = None, inverse=False
     ) -> torch.Tensor:
@@ -150,7 +197,7 @@ class Compressor:
                     )
                 )
 
-        transformed = torch.einsum(
+        transformed = torch.einsum(    #multiplying elements of tensor
             slope_indices.to(self.dtype),
             range(self.n_dimensions),
             transformer_tensor,
