Corrected space calculation

experiments/shallowwater.py
@@ -129,34 +129,34 @@ def draw_curves():
     #     linewidth=1,
     # )
 
-    plt.plot(np.asarray(timesteps), np.asarray(fastmath_vs_o3_norm2), label="fastmath vs O3",
+    plt.plot(np.asarray(timesteps), np.asarray(fastmath_vs_o3_norm2), label="FC1 vs FC0",
              # color=tab10[0]
              )
 
     plt.plot(
         np.asarray(timesteps),
         np.asarray(fastmath_vs_o3_norm2),
-        label="fastmath vs O3, compressed L2",
+        label="FC1 vs FC0, compressed L2",
         # color=tab10[7],
-        linewidth=1, linestyle=(0, (5, 5))
+        linestyle=(0, (5, 5))
     )
 
-    plt.plot(np.asarray(timesteps), np.asarray(ftz_vs_o3_norm2), label="ftz vs O3",
+    plt.plot(np.asarray(timesteps), np.asarray(ftz_vs_o3_norm2), label="FC2 vs FC0",
              # color=tab10[2]
              )
 
     plt.plot(
         np.asarray(timesteps),
         np.asarray(ftz_vs_o3_compressed_norm2),
-        label="ftz vs O3, compressed L2",
+        label="FC2 vs FC0, compressed L2",
         # color=tab10[5],
-        linewidth=1, linestyle=(0, (5, 5))
+        linestyle=(0, (5, 5))
     )
 
 
 draw_curves()
 plt.title("Magnitude of error in shallow water simulation")
-plt.xlabel("time step")
+plt.xlabel("time-step")
 plt.ylabel("magnitude of error")
 plt.legend()
 plt.savefig("results/ShallowWaters/ftz_fastmath_vs_o3_magnitude.pdf")
@@ -165,7 +165,7 @@ plt.savefig("results/ShallowWaters/ftz_fastmath_vs_o3_magnitude.pdf")
 plt.clf()
 draw_curves()
 plt.title("(Zoomed) Magnitude of error in shallow water simulation")
-plt.xlabel("time step")
+plt.xlabel("time-step")
 plt.ylabel("magnitude of error")
 plt.legend()
 plt.xticks(range(20), [str(x) for x in range(20)])

tools/plot_space.py
@@ -22,7 +22,7 @@ def main():
 def like_shallow_water():
     dimensions = 2
     tensor_size = (79, 55)
-    float_size = 64
+    float_size= 64
     int_size = 8
     block_size = 8
     n_time_steps = 10**6
@@ -33,7 +33,7 @@ def like_shallow_water():
         horizontal_values.append(time_step)
         uncompressed_sizes.append(time_step * uncompressed_size(tensor_size, float_size))
         compressed_sizes.append(
-            time_step * compressed_size(tensor_size, (block_size,) * dimensions, float_size, int_size)
+            time_step * compressed_size(tensor_size, (block_size,) * dimensions, 1, float_size, int_size)
         )
     uncompressed_sizes = np.array(uncompressed_sizes)
     compressed_sizes = np.array(compressed_sizes)
@@ -62,18 +62,22 @@ def sweep_settings(colors, ratio=False):
     tensor_sizes = [1 << p for p in range(2, 15)]
     float_sizes = (16, 64)
     int_sizes = (8, 32)
-    block_sizes = (4, 8, 16)
+    # block_sizes = (4, 8, 16)
+    block_size = 8
+    keep_rates = (1, 0.5)
     horizontal_values = [str(x) for x in tensor_sizes]
     for float_size in float_sizes:
         for int_size in int_sizes:
             plt.clf()
-            for line_style, n_dimensions in zip(("dotted", "dashed", "solid"), dimensions):
-                for color_index, block_size in enumerate(block_sizes):
+            for line_style, n_dimensions in zip(reversed(("dotted", "dashed", "solid")), reversed(dimensions)):
+                # for color_index, block_size in enumerate(block_sizes):
+                    # keep_rate = 1
+                for color_index, keep_rate in enumerate(keep_rates):
                     if ratio:
                         compression_ratio = [
-                            uncompressed_size((tensor_size,) * n_dimensions, float_size)
+                            uncompressed_size((tensor_size,) * n_dimensions)
                             / compressed_size(
-                                (tensor_size,) * n_dimensions, (block_size,) * n_dimensions, float_size, int_size
+                                (tensor_size,) * n_dimensions, (block_size,) * n_dimensions, keep_rate, float_size, int_size
                             )
                             for tensor_size in tensor_sizes
                         ]
@@ -82,11 +86,12 @@ def sweep_settings(colors, ratio=False):
                             compression_ratio,
                             color=colors[color_index],
                             linestyle=line_style,
-                            label=f"BS{block_size}, {n_dimensions}D",
+                            label=f"{n_dimensions}D, keep {int(keep_rate * 100)}%",
+                            # label=f"{n_dimensions}D, BS{block_size}",
                         )
                     else:
                         space_taken = [
-                            compressed_size((size,) * n_dimensions, (block_size,) * n_dimensions, float_size, int_size)
+                            compressed_size((size,) * n_dimensions, (block_size,) * n_dimensions, keep_rate, float_size, int_size)
                             for size in tensor_sizes
                         ]
                         plt.plot(
@@ -94,14 +99,15 @@ def sweep_settings(colors, ratio=False):
                             space_taken,
                             color=colors[color_index],
                             linestyle=line_style,
-                            label=f"BS{block_size}, {n_dimensions}D",
+                            label=f"{n_dimensions}D, keep {int(keep_rate * 100)}%",
+                            # label=f"{n_dimensions}D, BS{block_size}",
                         )
             if ratio:
                 plt.title(f"Compression ratio using float{float_size} and int{int_size}")
                 plt.xlabel("array size")
                 plt.xticks(rotation=-30)
                 plt.ylabel("uncompressed size / compressed size")
-                plt.yscale("log")
+                # plt.yscale("log")
                 plt.legend()
                 plt.tight_layout()
                 plt.savefig(save_path / f"ratio_float{float_size}_int{int_size}.pdf")
@@ -111,14 +117,14 @@ def sweep_settings(colors, ratio=False):
                 plt.xticks(rotation=-30)
                 plt.ylabel("compressed-array memory usage (bits)")
                 plt.yscale("log")
-                plt.ylim((10**2.5, 10**10))
+                # plt.ylim((10**2.5, 10**10))
                 plt.legend()
                 plt.tight_layout()
                 plt.savefig(save_path / f"space_float{float_size}_int{int_size}.pdf")
 
 
-def compressed_size(original_shape: tuple[int, ...], block_shape: tuple[int, ...], float_size: int, int_size: int):
-    n_blocks = sum(math.ceil(tensor_size / block_size) for tensor_size, block_size in zip(original_shape, block_shape))
+def compressed_size(original_shape: tuple[int, ...], block_shape: tuple[int, ...], keep_proportion: float, float_size: int, int_size: int):
+    n_blocks = math.prod(math.ceil(tensor_size / block_size) for tensor_size, block_size in zip(original_shape, block_shape))
     return (
         4
         + 64 * len(original_shape)
@@ -126,12 +132,12 @@ def compressed_size(original_shape: tuple[int, ...], block_shape: tuple[int, ...
         + 64 * len(original_shape)
         + math.prod(block_shape)
         + float_size * n_blocks
-        + int_size * math.prod(block_shape) * n_blocks
+        + int_size * keep_proportion * n_blocks * math.prod(block_shape)
     )
 
 
-def uncompressed_size(original_shape: tuple[int, ...], float_size: int):
-    return 2 + 64 * len(original_shape) + 64 + float_size * math.prod(original_shape)
+def uncompressed_size(original_shape: tuple[int, ...]):
+    return 64 * len(original_shape) + 64 + 64 * math.prod(original_shape)
 
 
 if __name__ == "__main__":