8ff922ef19ad664bda0060e1454182ac1418dd70
Harvey Dam <tanh@gatech.edu>
2022-10-04 11:14:24 -0600

    Labels in plots.

--- tools/plot_weighted_dct_shallow_water.py
+++ tools/plot_weighted_dct_shallow_water.py
@@ -13,20 +13,23 @@ def main():
     figure_path.mkdir(parents=True, exist_ok=True)
 
     colors = list(matplotlib.colors.TABLEAU_COLORS.keys())
-    
-    plt.plot(range(500), dataframe["fastmath_vs_o3"], color=colors[0])
-    plt.plot(range(500), dataframe["ftz_vs_o3"], color=colors[2])
-    plt.title("Absolute difference between sums of weighted absolute coefficients")
+
+    plt.plot(range(500), dataframe["fastmath_vs_o3"], color=colors[0], label="FC1 vs FC0")
+    plt.plot(range(500), dataframe["ftz_vs_o3"], color=colors[2], label="FC2 vs FC0")
+    plt.title("Absolute difference between sums of weighted absolute coefficients", wrap=True, loc="center")
     plt.ylabel("absolute difference")
     plt.xlabel("time-step")
+    plt.legend()
     plt.savefig("results/ShallowWaters/figures/weighted_dct_difference.pdf")
-    
+
     plt.clf()
-    plt.plot(range(20), dataframe["fastmath_vs_o3"].head(20), color=colors[0])
-    plt.plot(range(20), dataframe["ftz_vs_o3"].head(20), color=colors[2])
-    plt.title("(Zoomed) Absolute difference between sums of weighted absolute coefficients")
+    horizontal_values = [str(x) for x in range(20)]
+    plt.plot(horizontal_values, dataframe["fastmath_vs_o3"].head(20), color=colors[0], label="FC1 vs FC0")
+    plt.plot(horizontal_values, dataframe["ftz_vs_o3"].head(20), color=colors[2], label="FC2 vs FC0")
+    plt.title("(Zoomed) Absolute difference between sums of weighted absolute coefficients", wrap=True, loc="center")
     plt.ylabel("absolute difference")
     plt.xlabel("time-step")
+    plt.legend()
     plt.savefig("results/ShallowWaters/figures/weighted_dct_difference_xlim_0_19.pdf")
 
 
