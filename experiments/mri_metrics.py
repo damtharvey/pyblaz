f8bb1d770e60717662bd1d8e46d03d4e7065163a
Harvey Dam <harveytanh@gmail.com>
2023-01-03 23:30:25 -0700

    Added float types, int types, block sizes to MRI experiment

--- experiments/mri_metrics.py
+++ experiments/mri_metrics.py
@@ -19,31 +19,43 @@ def main():
     parser.add_argument("--results", type=str, default="../results")
     args = parser.parse_args()
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     data_path = pathlib.Path(args.data) / "lgg-mri-segmentation" / "as_tensors"
     assert data_path.exists(), "Run tools/mri_tif_to_tensor.py first."
     results_path = pathlib.Path(args.results) / "mri"
     results_path.mkdir(parents=True, exist_ok=True)
 
-    compressor = compression.Compressor((8, 8, 8))
+    for float_type in (torch.bfloat16, torch.float16, torch.float32, torch.float64):
+        for index_type in (torch.int8, torch.int16):
+            for block_size in (4, 8, 16):
 
-    to_write = [
-        "uncompressed_mean,compressed_mean,"
-        "uncompressed_variance,compressed_variance,"
-        "uncompressed_norm2,compressed_norm2"
-    ]
-    for example_path in tqdm.tqdm(tuple(data_path.glob("*")), desc="Measuring error"):
-        # According to the dataset README, some examples are missing channels. FLAIR is available in all examples.
-        flair = torch.load(example_path)[Channel.FLAIR]
-        compressed_flair = compressor.compress(flair)
+                compressor = compression.Compressor((block_size,) * 3, dtype=float_type, index_dtype=index_type)
 
-        to_write.append(
-            f"{flair.mean()},{compressed_flair.mean()},"
-            f"{flair.var(unbiased=False)},{compressed_flair.variance()},"
-            f"{flair.norm(2)},{compressed_flair.norm_2()}"
-        )
+                to_write = [
+                    "uncompressed_mean,compressed_mean,"
+                    "uncompressed_variance,compressed_variance,"
+                    "uncompressed_norm2,compressed_norm2"
+                ]
+                for example_path in tqdm.tqdm(
+                    tuple(data_path.glob("*")),
+                    desc=f"{str(float_type)[6:]} {str(index_type)[6:]} block size {block_size}.",
+                ):
+                    # According to the dataset README, some examples are missing channels.
+                    # FLAIR is available in all examples.
+                    flair = torch.load(example_path)[Channel.FLAIR].to(device)
+                    compressed_flair = compressor.compress(flair)
 
-    with open(results_path / "mri_metrics.csv", "w") as file:
-        file.write("\n".join(to_write))
+                    to_write.append(
+                        f"{flair.mean()},{compressed_flair.mean()},"
+                        f"{flair.var(unbiased=False)},{compressed_flair.variance()},"
+                        f"{flair.norm(2)},{compressed_flair.norm_2()}"
+                    )
+
+                with open(
+                    results_path / f"mri_metrics_{str(float_type)[6:]}_{str(index_type)[6:]}_bs{block_size}.csv", "w"
+                ) as file:
+                    file.write("\n".join(to_write))
 
 
 if __name__ == "__main__":
