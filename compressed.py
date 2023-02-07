fix blockwise variance and related

compressed.py
@@ -1,5 +1,4 @@
 import torch
-import math
 
 INDICES_RADIUS = {
     torch.int8: (1 << 7) - 1,
@@ -260,7 +259,7 @@ class CompressedTensor:
             return variance
 
     def standard_deviation(self, sample: bool = False) -> float:
-        return math.sqrt(self.variance)
+        return self.variance(sample) ** 0.5
 
     def blockwise_variance(self, sample: bool = False) -> torch.Tensor:
         """
@@ -279,9 +278,7 @@ class CompressedTensor:
                 * self.indicess.type(self.biggest_coefficients.dtype)
             )
 
-        coefficientss[..., 0] -= coefficientss[..., 0].sum() / torch.prod(torch.tensor(self.blocks_shape))
-
-        variance = (coefficientss**2).mean(axis=3)
+        variance = (coefficientss**2).mean(-1)
 
         if sample:
             return variance * (n_elements := torch.prod(torch.tensor(self.original_shape))) / (n_elements - 1)
@@ -289,7 +286,7 @@ class CompressedTensor:
             return variance
 
     def blockwise_standard_deviation(self, sample: bool = False) -> torch.Tensor:
-        return torch.sqrt(self.blockwise_variance)
+        return self.blockwise_variance(sample) ** 0.5
 
 
 if __name__ == "__main__":